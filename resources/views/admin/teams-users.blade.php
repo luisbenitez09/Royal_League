6a9903e2d206d56cc71732fb091eb7c9153b062c
mariameza99 <54654158+mariameza99@users.noreply.github.com>
2021-04-03 22:01:05 -0700

    Vista de usuarios - Admin

--- resources/views/admin/teams-users.blade.php
+++ resources/views/admin/teams-users.blade.php
@@ -12,8 +12,104 @@
     <title>Usuarios / Equipos</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
-<body>
-    <h1>holi</h1>
+<body class="bg-admin-dash bg-cove">
+    @livewire('admin-navbar')
+
+
+    <div class="max-w-6xl mx-auto bg-gray-500 bg-opacity-50 rounded-3xl my-20 p-10">
+                <h1 class="text-center text-2xl text-white font-bold mb-5">Usuarios registrados</h1>
+                <table class="w-full text-white">
+                    <thead class="text-xl border-b-2">
+                        <th class="w-1/2 pb-4 border-r-2">Equipo</th>
+                        <th class="pb-4 border-r-2">Puntos</th>
+                        <th class="pb-4">Posición</th>
+                    </thead>
+                    <tbody>
+                        <tr class="border-t-2">
+                            <td class="py-2 text-center border-r-2">Fouz</td>
+                            <td class="py-2 text-center border-r-2">1289</td>
+                            <td class="py-2 text-center">1</td>
+                        </tr>
+                        <tr class="border-t-2">
+                            <td class="py-2 text-center border-r-2">Momos 4k</td>
+                            <td class="py-2 text-center border-r-2">1287</td>
+                            <td class="py-2 text-center">2</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+    @livewire('footer')
+    <script>
+        function drop(x) {
+            if (x == 1) {
+                if (document.getElementById("drop-menu-1").classList.contains("hidden")) {
+                    document.getElementById("drop-menu-1").classList.remove("hidden");
+                } else {
+                    document.getElementById("drop-menu-1").classList.add("hidden");
+                }
+            } else {
+                if (document.getElementById("drop-menu-2").classList.contains("hidden")) {
+                    document.getElementById("drop-menu-2").classList.remove("hidden");
+                } else {
+                    document.getElementById("drop-menu-2").classList.add("hidden");
+                }
+            }
+
+
+        }
+
+        function toggle() {
+            var x = document.getElementById("nav-bar");
+            if (x.classList.contains("hidden")) {
+                x.classList.remove("hidden")
+                x.classList.add("block")
+            } else {
+                x.classList.remove("block")
+                x.classList.add("hidden")
+            }
+        }
+
+        window.onclick = function(event) {
+            if (!event.target.matches('.btn-drop')) {
+                var dropdown1 = document.getElementById("drop-menu-1");
+                var dropdown2 = document.getElementById("drop-menu-2");
+
+                if (!dropdown1.classList.contains('hidden')) {
+                    dropdown1.classList.add('hidden');
+                }
+                if (!dropdown2.classList.contains('hidden')) {
+                    dropdown2.classList.add('hidden');
+                }
+
+            }
+        }
+
+        function openSidePanel() {
+            var x = document.getElementById("sidePanel");
+            x.classList.remove("hidden")
+            x.classList.add("fixed")
+        }
+
+        function closeSidePanel() {
+            var x = document.getElementById("sidePanel");
+            x.classList.remove("fixed")
+            x.classList.add("hidden")
+        }
+
+        function openSidePanelJoin() {
+            var x = document.getElementById("sidePanelJoin");
+            x.classList.remove("hidden")
+            x.classList.add("fixed")
+        }
+
+        function closeSidePanelJoin() {
+            var x = document.getElementById("sidePanelJoin");
+            x.classList.remove("fixed")
+            x.classList.add("hidden")
+        }
+    </script>
     
 </body>
 </html>
